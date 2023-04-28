7d33f5bed6bb202f3a444d8c787c9a31205d9c05
Miloslav Nosek <6146756+miloslavnosek@users.noreply.github.com>
2023-04-28 13:22:38 +0200

    feat: add simple add task input

--- ui/ui.go
+++ ui/ui.go
@@ -2,14 +2,17 @@ package ui
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"planner/task"
 	"strconv"
 )
 
 type Model struct {
-	taskList list.Model
+	taskList  list.Model
+	taskInput textinput.Model
 }
 
 type taskItem task.Task
@@ -17,6 +20,8 @@ type taskItem task.Task
 func (i taskItem) Title() string       { return i.Name }
 func (i taskItem) Description() string { return i.Desc }
 func (i taskItem) FilterValue() string { return strconv.FormatInt(i.ID, 10) }
+
+var database *sql.DB
 
 func ConvertTasksToItems(tasks []task.Task) []list.Item {
 	items := make([]list.Item, len(tasks))
@@ -26,15 +31,31 @@ func ConvertTasksToItems(tasks []task.Task) []list.Item {
 	return items
 }
 
+func reloadTasks(m *Model, database *sql.DB) {
+	m.taskInput.Reset()
+	storedTasks, _ := task.GetTasks(database)
+	taskItems := ConvertTasksToItems(storedTasks)
+	m.taskList.SetItems(taskItems)
+}
+
 func InitialModel(db *sql.DB) Model {
 	storedTasks, _ := task.GetTasks(db)
 	taskItems := ConvertTasksToItems(storedTasks)
+	database = db
 
 	m := Model{
-		taskList: list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0),
+		taskList:  list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 20),
+		taskInput: textinput.New(),
 	}
+
 	m.taskList.SetShowTitle(false)
 	m.taskList.SetItems(taskItems)
+	m.taskList.SetShowHelp(false)
+	m.taskList.KeyMap.CursorUp.SetKeys("up")
+	m.taskList.KeyMap.CursorDown.SetKeys("down")
+
+	m.taskInput.Focus()
+	m.taskList.DisableQuitKeybindings()
 
 	return m
 }
@@ -44,22 +65,37 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q":
-			return m, tea.Quit
+		if m.taskInput.Focused() {
+			switch msg.String() {
+			case "enter":
+				_, err := task.AddTask(database, task.Task{Name: m.taskInput.Value()})
+				if err != nil {
+					fmt.Sprintf("Error adding task: %v", err)
+				} else {
+					reloadTasks(&m, database)
+				}
+			}
+		} else {
+			switch msg.String() {
+			case "w":
+				return m, tea.Quit
+			}
 		}
 	case tea.WindowSizeMsg:
 		m.taskList.SetWidth(msg.Width)
 		return m, nil
 	}
 
-	var cmd tea.Cmd
 	m.taskList, cmd = m.taskList.Update(msg)
+	m.taskInput, _ = m.taskInput.Update(msg)
+
 	return m, cmd
 }
 
 func (m Model) View() string {
-	return m.taskList.View()
+	return m.taskList.View() + "\n" + m.taskInput.View()
 }
