bf7593188e10ba3254dcdeb036061d8c6cdbac5d
Miloslav Nosek <6146756+miloslavnosek@users.noreply.github.com>
2023-04-28 22:03:05 +0200

    feat: add dynamic focus-based border colors for containers

--- ui/ui.go
+++ ui/ui.go
@@ -6,6 +6,7 @@ import (
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"planner/task"
 	"strconv"
 )
@@ -13,15 +14,36 @@ import (
 type Model struct {
 	taskList  list.Model
 	taskInput textinput.Model
+
+	windowWidth  int
+	windowHeight int
 }
 
 type taskItem task.Task
 
 func (i taskItem) Title() string       { return i.Name }
 func (i taskItem) Description() string { return i.Desc }
-func (i taskItem) FilterValue() string { return strconv.FormatInt(i.ID, 10) }
+func (i taskItem) FilterValue() string { return strconv.FormatInt(i.ID, 20) }
 
-var database *sql.DB
+var (
+	database *sql.DB
+
+	inputContainerStyle lipgloss.Style
+	listContainerStyle  lipgloss.Style
+)
+
+func createContainerStyle(focused bool, width, height int) lipgloss.Style {
+	borderColor := lipgloss.Color("#CCC")
+	if focused {
+		borderColor = lipgloss.Color("#FF5733")
+	}
+
+	return lipgloss.NewStyle().
+		Border(lipgloss.NormalBorder()).
+		BorderForeground(borderColor).
+		Width(width).
+		Height(height)
+}
 
 func ConvertTasksToItems(tasks []task.Task) []list.Item {
 	items := make([]list.Item, len(tasks))
@@ -39,23 +61,32 @@ func reloadTasks(m *Model, database *sql.DB) {
 }
 
 func InitialModel(db *sql.DB) Model {
-	storedTasks, _ := task.GetTasks(db)
-	taskItems := ConvertTasksToItems(storedTasks)
 	database = db
 
+	storedTasks, _ := task.GetTasks(db)
+	taskItems := ConvertTasksToItems(storedTasks)
+
 	m := Model{
-		taskList:  list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 20),
-		taskInput: textinput.New(),
+		taskList:     list.New([]list.Item{}, list.NewDefaultDelegate(), 80, 20),
+		taskInput:    textinput.New(),
+		windowWidth:  120,
+		windowHeight: 20,
 	}
 
 	m.taskList.SetShowTitle(false)
 	m.taskList.SetItems(taskItems)
 	m.taskList.SetShowHelp(false)
+	m.taskList.SetShowStatusBar(false)
 	m.taskList.KeyMap.CursorUp.SetKeys("up")
 	m.taskList.KeyMap.CursorDown.SetKeys("down")
+	m.taskList.KeyMap.PrevPage.SetKeys()
+	m.taskList.KeyMap.NextPage.SetKeys()
 
 	m.taskInput.Focus()
 	m.taskList.DisableQuitKeybindings()
+
+	inputContainerStyle = createContainerStyle(m.taskInput.Focused(), 80, 3)
+	listContainerStyle = createContainerStyle(!m.taskInput.Focused(), 80, 3)
 
 	return m
 }
@@ -71,31 +102,44 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		if m.taskInput.Focused() {
 			switch msg.String() {
-			case "enter":
+			case "ctrl+enter":
 				_, err := task.AddTask(database, task.Task{Name: m.taskInput.Value()})
 				if err != nil {
 					fmt.Sprintf("Error adding task: %v", err)
 				} else {
 					reloadTasks(&m, database)
 				}
+			case "up", "esc":
+				m.taskInput.Blur()
 			}
-		} else {
+		} else if !m.taskInput.Focused() {
 			switch msg.String() {
-			case "w":
-				return m, tea.Quit
+			case "down":
+				if m.taskList.Cursor() == len(m.taskList.Items())-1 {
+					m.taskInput.Focus()
+				}
 			}
 		}
+
+		switch msg.String() {
+		case "ctrl+q":
+			return m, tea.Quit
+		}
 	case tea.WindowSizeMsg:
-		m.taskList.SetWidth(msg.Width)
+		m.windowWidth = msg.Width
+		m.windowHeight = msg.Height
 		return m, nil
 	}
 
-	m.taskList, cmd = m.taskList.Update(msg)
+	m.taskList, _ = m.taskList.Update(msg)
 	m.taskInput, _ = m.taskInput.Update(msg)
+
+	inputContainerStyle = createContainerStyle(m.taskInput.Focused(), 80, 3)
+	listContainerStyle = createContainerStyle(!m.taskInput.Focused(), 80, 3)
 
 	return m, cmd
 }
 
 func (m Model) View() string {
-	return m.taskList.View() + "\n" + m.taskInput.View()
+	return listContainerStyle.Render(m.taskList.View()) + "\n" + inputContainerStyle.Render(m.taskInput.View())
 }
