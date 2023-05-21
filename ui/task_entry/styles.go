568592649fe8493ecd9ebf66cb435a5c5a6d328d
Miloslav Nosek <6146756+miloslavnosek@users.noreply.github.com>
2023-05-21 16:15:02 +0200

    feat(task-entry): recolor name and input styles

---
+++ ui/task_entry/styles.go
@@ -0,0 +1,8 @@
+package task_entry
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	nameInputPlaceholderStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("#585b70"))
+	descriptionInputPlaceholderStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#585b70"))
+)
